d4b1fdb176b128a9014c0e91efc8395130b8824e
joeyappelhans <9jappelhans@stpatrick.org>
2016-10-07 12:56:39 -0500

    octocat almost done

---
+++ octocats.js
@@ -0,0 +1,14 @@
+//Write your constructor function below.
+//You should have 2 instance variables and 1 instance function included.
+
+
+
+
+
+
+
+
+
+
+
+//Write your 3 new octocat objects below here.
